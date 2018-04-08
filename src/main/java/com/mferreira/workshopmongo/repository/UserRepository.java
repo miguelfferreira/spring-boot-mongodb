4e045279e43ce8dfa4d11c9121bb238e54be66a2
Miguel Ferreira <miguelfferreira93@gmail.com>
2018-04-08 20:50:06 +0100

    Handler

--- src/main/java/com/mferreira/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/mferreira/workshopmongo/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.mferreira.workshopmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mferreira.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String>{
-
+	
 }
