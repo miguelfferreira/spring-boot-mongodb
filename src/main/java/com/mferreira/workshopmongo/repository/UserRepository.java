475cb96e4bab79c5571cce114fb53610c781b3ba
Miguel Ferreira <miguelfferreira93@gmail.com>
2018-04-09 20:59:03 +0100

    Inser

--- src/main/java/com/mferreira/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/mferreira/workshopmongo/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.mferreira.workshopmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mferreira.workshopmongo.domain.User;
