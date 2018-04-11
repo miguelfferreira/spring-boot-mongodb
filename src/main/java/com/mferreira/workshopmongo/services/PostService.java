4ee805625721b1c35fd9f9087e34896c0bcbbb4e
Miguel Ferreira <miguelfferreira93@gmail.com>
2018-04-11 21:52:37 +0100

    findById

---
+++ src/main/java/com/mferreira/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.mferreira.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mferreira.workshopmongo.domain.Post;
+import com.mferreira.workshopmongo.repository.PostRepository;
+import com.mferreira.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
